b847b5e952fc75d99fd2624b734cf1c4875ed980
Tosin-Mathew <emmanueltosinmathew@gmail.com>
2025-09-21 18:25:52 +0100

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,33 @@
+// src/data/movies.js
+export const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    image: "https://via.placeholder.com/300x450/0077be/ffffff?text=Inception",
+    description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    trailerLink: "https://www.youtube.com/embed/YoHD9XEInc0",
+    year: 2010,
+    rating: "PG-13",
+    duration: "2h 28m"
+  },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    image: "https://via.placeholder.com/300x450/333333/ffffff?text=The+Dark+Knight",
+    description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    trailerLink: "https://www.youtube.com/embed/EXeTwQWrcwY",
+    year: 2008,
+    rating: "PG-13",
+    duration: "2h 32m"
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    image: "https://via.placeholder.com/300x450/2c3e50/ffffff?text=Interstellar",
+    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    trailerLink: "https://www.youtube.com/embed/zSWdZVtXT7E",
+    year: 2014,
+    rating: "PG-13",
+    duration: "2h 49m"
+  }
+];
